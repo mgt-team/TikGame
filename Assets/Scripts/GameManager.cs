fc72a859d7b25cd86ebd055d3e2b6425f39cf559
Max Barashev <tushkevich.ma@gmail.com>
2018-08-23 01:10:55 +0300

    Add scripts GameManager and FinishPlatform

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    [SerializeField]
+    private FinishPlatform finishPlatform;
+
+    private void Start()
+    {
+        finishPlatform.onFinish += FinishPlatform_onFinish;
+    }
+
+    private void FinishPlatform_onFinish()
+    {
+        Debug.Log("Finish");
+    }
+}
---
+++ Assets/Scripts/Platform/FinishPlatform.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishPlatform : Platform {
+
+    public delegate void MethoodContainer();
+    public event MethoodContainer onFinish;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.name == "Player")
+        {
+            onFinish();
+        }
+            
+    }
+}
