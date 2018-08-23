821ae5b2b9dd0436cdcbf2ffabda8780ef24b10c
Nikita Chernyakov <nikchernyakov@yandex.ru>
2018-08-23 23:24:55 +0300

    Add controller to generate new platform by button clicked

---
+++ Assets/Scripts/Platform/PlatformGenerationByButtonController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class PlatformGenerationByButtonController : MonoBehaviour {
+
+	[SerializeField]
+	private PlatformManager _platformManager;
+	
+	// Update is called once per frame
+	private void Update () {
+		if (Input.GetKeyDown(KeyCode.G))
+		{
+			_platformManager.EnableRandomPlatform();
+		}
+	}
+}
