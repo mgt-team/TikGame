5b7b9673e64c09fffa7ea422e17b4fa10ca83eef
Nikita Chernyakov <nikchernyakov@yandex.ru>
2018-08-23 23:23:01 +0300

    Implement platform generation
    - Create generation methods in PlatformManager
    - Add neighbors to Platform and methods to work with them
    - Add PlatformMap to group all Platforms

---
+++ Assets/Scripts/Managers/PlatformManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformManager : MonoBehaviour
+{
+	[SerializeField] 
+	private PlatformMap _platformMap;
+	[SerializeField] 
+	private Platform _finishPlatform;
+	[SerializeField] 
+	private List<Platform> _startPlatforms;
+
+	public void InitPlatformMap()
+	{
+		// Disable all platforms on map
+		foreach (var platform in _platformMap.GetPlatforms())
+		{
+			platform.Disable();
+		}
+		
+		
+		// Enable start and finish platforms
+		_finishPlatform.Enable();
+		foreach (var platform in _startPlatforms)
+		{
+			platform.Enable();
+		}
+	}
+
+	public void EnableRandomPlatform()
+	{
+		var enabledPlatform = GetRandomEnabledPlatformWithDisabledNeighbor();
+		
+		// Do nothing if there is no any disabled platforms in game
+		if (enabledPlatform == null)
+		{
+			return;
+		}
+
+		enabledPlatform.GetRandomDisabledPlatform().Enable();
+	}
+
+	private Platform GetRandomEnabledPlatformWithDisabledNeighbor()
+	{
+		var enabledPlatforms = GetEnabledPlatformsWithDisabledNeighbor();
+
+		// Check if there is no any disabled platforms in game
+		if (enabledPlatforms.Count == 0)
+		{
+			Debug.Log("There is no any disabled platforms in game to enable it");
+			return null;
+		}
+		else
+		{
+			return enabledPlatforms[Random.Range(0, enabledPlatforms.Count)];	
+		}
+	}
+
+	private List<Platform> GetEnabledPlatformsWithDisabledNeighbor()
+	{
+		return _platformMap.GetPlatforms()
+			.FindAll(platform => platform.IsEnabled() && platform.HasDisabledNeighbor());
+	}
+}
--- Assets/Scripts/Platform/Platform.cs
+++ Assets/Scripts/Platform/Platform.cs
@@ -1,15 +1,47 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 
-public class Platform : MonoBehaviour {
+public class Platform : MonoBehaviour
+{
+    
+    [SerializeField]
+    private List<Platform> _neighborsList;
 
+    private bool _isEnabled = false;
+    
     public void Enable()
     {
+        _isEnabled = true;
         gameObject.SetActive(true);
     }
     
     public void Disable()
     {
+        _isEnabled = false;
         gameObject.SetActive(false);
     }
-    
+
+    public bool IsEnabled()
+    {
+        return _isEnabled;
+    }
+
+    public bool HasDisabledNeighbor()
+    {
+        return _neighborsList.Any(neighbor => neighbor.IsEnabled() == false);
+    }
+
+    public Platform GetRandomDisabledPlatform()
+    {
+        var disabledNeighbors = _neighborsList.FindAll(neighbor => neighbor.IsEnabled() == false);
+
+        if (disabledNeighbors.Count == 0)
+        {
+            Debug.LogWarning("Tries to get disabled platform from platform without disabled neighbor");
+            return null;
+        }
+
+        return disabledNeighbors[Random.Range(0, disabledNeighbors.Count)];
+    }
 }
