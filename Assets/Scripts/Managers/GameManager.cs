c0d5a3a11e0f9eb75bc09964f1941d843b07c883
Nikita Chernyakov <nikchernyakov@yandex.ru>
2018-08-23 23:24:32 +0300

    Replace GameManager and add initialization platform map

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    [SerializeField]
+    private FinishPlatform _finishPlatform;
+    
+    [SerializeField]
+    private PlatformManager _platformManager;
+
+    private void Start()
+    {
+        _finishPlatform.onFinish += FinishPlatform_onFinish;
+        
+        _platformManager.InitPlatformMap();
+    }
+
+    private void FinishPlatform_onFinish()
+    {
+        Debug.Log("Finish");
+    }
+}
