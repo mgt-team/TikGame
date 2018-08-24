ab2368e961e0eb647a39e4ea0047874bee17b312
Nikita Chernyakov <nikchernyakov@yandex.ru>
2018-08-25 01:07:38 +0300

    Add choosing mouse button for building platform

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,16 +1,20 @@
 ﻿using UnityEngine;
+using UnityEngine.Experimental.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
 
 	[SerializeField]
 	private DirectionOnPlatformController _directionOnPlatformController;
+
+	[SerializeField] 
+	private MouseButton _mouseButton;
 	
 	// Update is called once per frame
 	private void Update ()
 	{
 		var targetPlatform = _directionOnPlatformController.GetTargetPlatform();
-		if (Input.GetMouseButtonDown(1) && targetPlatform != null 
+		if (Input.GetMouseButtonDown(_mouseButton.GetHashCode()) && targetPlatform != null 
 		                                && !targetPlatform.IsEnabled())
 		{
 			targetPlatform.Enable();
