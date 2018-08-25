6bf03205bcbd4e2a473a3bb6e7dd0ad7a6816fd9
Nikita Chernyakov <nikchernyakov@yandex.ru>
2018-08-25 12:07:10 +0300

    Create bullet script

---
+++ Assets/Scripts/Bullet/Bullet.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+	private Destroyble _destroyble;
+
+	private void Awake()
+	{
+		_destroyble = GetComponent<Destroyble>();
+	}
+
+	private void OnCollisionEnter(Collision other)
+	{
+		if (other.gameObject.CompareTag(TagManager.GetTagNameByEnum(TagEnum.Platform)))
+		{
+			var platform = other.gameObject.GetComponent<Platform>();
+			OnPlatformCollision(platform);
+		}
+	}
+
+	private void OnPlatformCollision(Platform platform)
+	{
+		platform.Disable();
+		_destroyble.Destroy();
+	}
+}
---
+++ Assets/Scripts/Bullet/Destroyble/Destroyble.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+
+public abstract class Destroyble : MonoBehaviour
+{
+	public abstract void Destroy();
+}
