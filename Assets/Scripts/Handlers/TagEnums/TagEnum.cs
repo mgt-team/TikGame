5fbae4ff9314e37fb8a25a2afc6dedee749ce775
Nikita Chernyakov <nikchernyakov@yandex.ru>
2018-08-25 13:47:01 +0300

    Update tag manager
    - Add method to compare tag with tag list
    - Add tag for stable platform

--- Assets/Scripts/Handlers/TagEnums/TagEnum.cs
+++ Assets/Scripts/Handlers/TagEnums/TagEnum.cs
@@ -1,5 +1,6 @@
 ﻿public enum TagEnum
 {
     Player, 
-    Platform
+    Platform,
+    StablePlatform
 }
--- Assets/Scripts/Handlers/TagManager.cs
+++ Assets/Scripts/Handlers/TagManager.cs
@@ -1,11 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
-public class TagManager
+public static class TagManager
 {
 
     public static string GetTagNameByEnum<T>(T value)
     {
         return Enum.GetName(typeof(T), value);
+    }
+
+    public static bool CompareTagWithTagsList<T>(string tagToCompare, IEnumerable<T> tagEnumList)
+    {
+        return tagEnumList.Select(GetTagNameByEnum)
+            .Any(tag => tag.Equals(tagToCompare));
     }
 }
 
