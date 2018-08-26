91fc4f418a67a120bb1a5b2f839e5814f10f9096
Nikita Chernyakov <nikchernyakov@yandex.ru>
2018-08-26 22:37:15 +0300

    Update PlayerController
    -  Rename variables

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -11,20 +11,20 @@ public class PlayerController : MonoBehaviour
     private Gun _gun;
 
     [SerializeField]
-    private MouseButton _mouseButtonForGenerate;
+    private MouseButton _generateMouseButton;
 
     [SerializeField]
-    private MouseButton _mouseButtonForShoot;
+    private MouseButton _shootMouseButton;
 
     public delegate void MethoodContainer();
     public event MethoodContainer ActionCommitted;
 
     [SerializeField]
-    private float _interval;
+    private float _shootCooldown;
 
     [ReadOnly]
     [SerializeField]
-    private float _timer = 0;
+    private float _shootTimer = 0;
 
     private void Start()
     {
@@ -33,34 +33,34 @@ public class PlayerController : MonoBehaviour
 
     private void PlayerController_ActionCommitted()
     {
-        _timer = _interval;
+        _shootTimer = _shootCooldown;
     }
 
     private void Update()
     {
-        if (_timer <= 0)
+        if (_shootTimer <= 0)
         {
             _directionOnPlatformController.WhiteMaterial();
             ApproveOfAction();
         }
-        else if (_timer > 0)
+        else if (_shootTimer > 0)
         {
             _directionOnPlatformController.RedMaterial();
-            _timer -= Time.deltaTime;
+            _shootTimer -= Time.deltaTime;
         }
     }
 
     public void ApproveOfAction()
     {
         var targetPlatform = _directionOnPlatformController.GetTargetPlatform();
-        if (Input.GetMouseButton(_mouseButtonForGenerate.GetHashCode()) && targetPlatform != null
+        if (Input.GetMouseButton(_generateMouseButton.GetHashCode()) && targetPlatform != null
                                         && !targetPlatform.IsEnabled())
         {
             targetPlatform.Enable();
             ActionCommitted();
         }
 
-        if (Input.GetMouseButton(_mouseButtonForShoot.GetHashCode()))
+        if (Input.GetMouseButton(_shootMouseButton.GetHashCode()))
         {
             _gun.Shoot();
             ActionCommitted();
